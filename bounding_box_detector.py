9168a120d76405738c2c52f6e0f0e0b067bb4d95
Rishith Seelam <rseelam@umich.edu>
2024-04-21 17:23:46 -0400

    initial structure 4

--- bounding_box_detector.py
+++ bounding_box_detector.py
@@ -11,7 +11,14 @@ def main():
     
     print("This is a module that, given an image, returns an array of bounding boxes")
 
-def grab_bounding_boxes():
+def grab_bounding_boxes(img):
+    # returns an np array of c1, c2, c3, c4 which are the corner in clockwise order from TL
+    # 
+    #   C1   C2
+    #
+    #   C4   C3
+    #
+    # image must be an np_array of size H,W with values of 0 or 1 
     pass
 
 
---
+++ utilty.py
@@ -0,0 +1,31 @@
+"""
+Data Preprocessing Script
+
+- enumerates data in the raw_datasets/ and places stardized ouput pickles in the processes_dataset folder
+- converts to standard format (jpeg)
+- drops unsupported characters
+- converts to custom bool encoding format
+"""
+
+def main():
+    
+    print("This is a ulitiy module that")
+
+def compress_image_path(img_path):
+    # returns an np array of a a image compressed with out compression format
+    # expects an image in the form
+  
+
+    pass
+
+def compress_image(img_path):
+    # returns an np array of a a image compressed with out compression format
+    # expects an np array of size H,W,3
+    
+  
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
