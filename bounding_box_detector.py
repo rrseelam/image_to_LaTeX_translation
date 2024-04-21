77160b1462edc8ef292170575824826d5b105b99
Rishith Seelam <rseelam@umich.edu>
2024-04-21 17:10:22 -0400

    initial structure 3

---
+++ bounding_box_detector.py
@@ -0,0 +1,19 @@
+"""
+Data Preprocessing Script
+
+- enumerates data in the raw_datasets/ and places stardized ouput pickles in the processes_dataset folder
+- converts to standard format (jpeg)
+- drops unsupported characters
+- converts to custom bool encoding format
+"""
+
+def main():
+    
+    print("This is a module that, given an image, returns an array of bounding boxes")
+
+def grab_bounding_boxes():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,8 +1,23 @@
 """
 Data Preprocessing Script
 
-- enumerates data in the raw_datasets/ and places stardized ouput 
+- enumerates data in the raw_datasets/ and places stardized ouput pickles in the processes_dataset folder
+- converts to standard format (jpeg)
+- drops unsupported characters
+- converts to custom bool encoding format
 """
+
+import os
+
+def main():
+
+    root_dir = "/raw_datasets"
+
+    for folder, _, files in os.walk(root_dir):
+        pass
+
+if __name__ == "__main__":
+    main()
 
 
 
