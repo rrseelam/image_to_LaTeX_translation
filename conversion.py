a1c80dffd2a31f835240aa54b761cb87fec6d52b
Rishith Seelam <rseelam@umich.edu>
2024-04-24 15:18:08 -0400

    started conversion

---
+++ conversion.py
@@ -0,0 +1,30 @@
+
+
+def main():
+    print("Testing Code")
+    pass
+
+def predict(img):
+    # given an img (32 x 32) return predicted class
+    return -1
+
+
+
+def convert_equation_to_latex():
+
+    # get bounding boxes
+
+    # for box in bounding boxes
+    #   predict each character 
+    #   put in a map 
+    #   
+    return -1
+
+import os
+os.remove("./models")
+
+
+
+
+if __name__ == "__main__":
+    main()
